266c58b6ef14290749644b786ad601c94500835b
patpalmerston <patpalmerston@gmail.com>
2019-04-17 16:04:10 -0700

    actions

---
+++ friends/src/actions/index.js
@@ -0,0 +1,22 @@
+//actions
+import axios from 'axios'
+
+export const FETCH_FRIEND_START = 'FETCH_FRIEND_START';
+export const FETCH_FRIEND_SUCCESS = 'FETCH_FRIEND_SUCCESS';
+export const FETCH_FRIEND_FAILURE = 'FETCH_FRIEND_FAILURE';
+
+
+export const getFriends = () => dispatch => {
+  dispatch({ tyep: FETCH_FRIEND_START });
+
+  axios.get('http://localhost:5000/api/friends')
+    .then(res => dispatch({
+      type: FETCH_FRIEND_SUCCESS,
+      payload: res.data
+    }))
+    
+    .catch(err => dispatch({
+      type: FETCH_FRIEND_FAILURE,
+      payload: 'Erro! Unable to Fetch API'
+    }))
+}
